c6c1005723abcb4ed71a263b70cd9929b34fd2d8
kkkkkse <eun2abc@gmail.com>
2023-05-11 11:39:55 +0900

    프로그래머스 레벨2 - 소수 찾기(재귀함수)

---
+++ FindPrime/solution.js
@@ -0,0 +1,37 @@
+function checkPrime(n) {
+    const num = Number(n);
+
+    if(num < 2) return false;
+
+    for(let i = 2; i <= Math.sqrt(num); i++){
+      if(num % i === 0){
+        return false;
+      }
+    }
+    return true;
+}
+
+function helper(set, str, visitArr, numbers, len){
+    for(let i = 0; i < numbers.length; i++){
+        if(visitArr[i]) continue;
+        visitArr[i] = true;
+        set.add(str + numbers[i]);
+        helper(set, str + numbers[i], visitArr, numbers, len);
+        visitArr[i] = false;
+    }
+}
+
+function solution(numbers) {
+    const set = new Set();
+    const len = numbers.length;
+    const visited = new Array(len).fill(false);
+    const answer = new Set();
+
+    helper(set, '', visited, numbers, len);
+
+    for(let el of set){
+        if(checkPrime(el)) answer.add(el);
+    }
+    
+    return answer.size;
+}
